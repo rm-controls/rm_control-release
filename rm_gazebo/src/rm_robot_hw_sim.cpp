b8255f8cb133f409e974330167df4b636b94e53a
qiayuan <liaoqiayuan@gmail.com>
2021-08-12 15:37:59 +0800

    Revert "Trimmed the branch to only the rm_common sub directory"
    This reverts commit 5b8db69a8a8e17875d0fd29a73ef7c5d990ceaad.

---
+++ rm_gazebo/src/rm_robot_hw_sim.cpp
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Qiayuan Liao
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+ 
+//
+// Created by qiayuan on 2/10/21.
+//
+
+#include "rm_gazebo/rm_robot_hw_sim.h"
+
+#include <gazebo_ros_control/gazebo_ros_control_plugin.h>
+
+namespace rm_gazebo {
+
+bool RmRobotHWSim::initSim(const std::string &robot_namespace,
+                           ros::NodeHandle model_nh,
+                           gazebo::physics::ModelPtr parent_model,
+                           const urdf::Model *urdf_model,
+                           std::vector<transmission_interface::TransmissionInfo> transmissions) {
+  bool ret = DefaultRobotHWSim::initSim(robot_namespace, model_nh, parent_model, urdf_model, transmissions);
+  gazebo_ros_control::DefaultRobotHWSim::registerInterface(&robot_state_interface_);
+  return ret;
+}
+
+void RmRobotHWSim::readSim(ros::Time time, ros::Duration period) {
+  gazebo_ros_control::DefaultRobotHWSim::readSim(time, period);
+  // Set cmd to zero to avoid crazy soft limit oscillation when not controller loaded
+  for (auto &cmd:joint_effort_command_)
+    cmd = 0;
+  for (auto &cmd:joint_velocity_command_)
+    cmd = 0;
+}
+
+}
+
+PLUGINLIB_EXPORT_CLASS(rm_gazebo::RmRobotHWSim, gazebo_ros_control::RobotHWSim)
+GZ_REGISTER_MODEL_PLUGIN(gazebo_ros_control::GazeboRosControlPlugin) // Default plugin
