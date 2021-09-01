22722a44916daba1298cdfe4bfa5d3bac6ddab5c
qiayuan <liaoqiayuan@gmail.com>
2021-09-01 12:51:32 +0000

    Imported upstream version '0.1.3' of 'upstream'

--- rm_common/include/rm_common/filters/one_euro_filter.h
+++ rm_common/include/rm_common/filters/one_euro_filter.h
@@ -30,21 +30,22 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
- 
+
 //
 // Created by liucong on 2020/12/5.
 //
 
-#ifndef RM_COMMON_FILTERS_ONE_EURO_FILTER_H
-#define RM_COMMON_FILTERS_ONE_EURO_FILTER_H
+#pragma once
 
 #include <cmath>
 
-template<typename T>
-class OneEuroFilter {
- public:
+template <typename T>
+class OneEuroFilter
+{
+public:
   OneEuroFilter(double _freq, T _mincutoff, T _beta, T _dcutoff)
-      : freq(_freq), mincutoff(_mincutoff), beta(_beta), dcutoff(_dcutoff) {
+    : freq(_freq), mincutoff(_mincutoff), beta(_beta), dcutoff(_dcutoff)
+  {
     firsttime = true;
     x_prev = 0;
     hatxprev = 0;
@@ -54,7 +55,8 @@ class OneEuroFilter {
 
   ~OneEuroFilter() = default;
 
-  void input(T input_value) {
+  void input(T input_value)
+  {
     T dx = 0;
     if (!firsttime)
       dx = (input_value - x_prev) * freq;
@@ -71,29 +73,30 @@ class OneEuroFilter {
     firsttime = false;
   };
 
-  T alpha(T cutoff, double freq) {
+  T alpha(T cutoff, double freq)
+  {
     T tau = 1.0 / (2 * M_PI * cutoff);
     T te = 1.0 / freq;
     return 1.0 / (1.0 + tau / te);
   }
 
-  T output() {
+  T output()
+  {
     return filtered_val;
   };
 
-  void clear() {
+  void clear()
+  {
     firsttime = true;
     x_prev = 0;
     hatxprev = 0;
     dhatxprev = 0;
   };
 
- private:
+private:
   double freq;
   bool firsttime;
   T mincutoff, beta, dcutoff;
   T x_prev, dhatxprev, hatxprev;
   T filtered_val;
 };
-
-#endif // RM_COMMON_FILTERS_ONE_EURO_FILTER_H
