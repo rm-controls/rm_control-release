6549b601ecefedddbefd603f9807e3059283316e
qiayuan <liaoqiayuan@gmail.com>
2021-09-01 12:53:45 +0000

    Rebase from 'debian/noetic/rm_common'

--- include/rm_common/math_utilities.h
+++ include/rm_common/math_utilities.h
@@ -30,17 +30,17 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
- 
+
 //
 // Created by qiayuan on 12/22/19.
 //
 
-#ifndef RM_COMMON_MATH_UTILITIES_H
-#define RM_COMMON_MATH_UTILITIES_H
+#pragma once
 
 #include <cmath>
-template<typename T>
-T angularMinus(T a, T b) {
+template <typename T>
+T angularMinus(T a, T b)
+{
   a = fmod(a, 2.0 * M_PI);
   b = fmod(b, 2.0 * M_PI);
 
@@ -50,27 +50,29 @@ T angularMinus(T a, T b) {
   return (std::abs(res1) < std::abs(res2)) ? res1 : res2;
 }
 
-template<typename T>
-T minAbs(T a, T b) {
+template <typename T>
+T minAbs(T a, T b)
+{
   T sign = (a < 0.0) ? -1.0 : 1.0;
   return sign * fmin(fabs(a), b);
 }
 
-template<typename T>
-int sgn(T val) {
+template <typename T>
+int sgn(T val)
+{
   return (T(0) < val) - (val < T(0));
 }
 
-template<typename T>
-T square(T val) {
+template <typename T>
+T square(T val)
+{
   return val * val;
 }
 
-template<typename T>
-T alpha(T cutoff, double freq) {
+template <typename T>
+T alpha(T cutoff, double freq)
+{
   T tau = 1.0 / (2 * M_PI * cutoff);
   T te = 1.0 / freq;
   return 1.0 / (1.0 + tau / te);
 }
-
-#endif // RM_COMMON_MATH_UTILITIES_H
