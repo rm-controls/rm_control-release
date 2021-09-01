aced75f1ab6a998806871f8279642dfa22edf625
qiayuan <liaoqiayuan@gmail.com>
2021-09-01 12:55:55 +0000

    Rebase from 'release/noetic/rm_common'

--- include/rm_common/decision/target_cost_function.h
+++ include/rm_common/decision/target_cost_function.h
@@ -30,13 +30,12 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
- 
+
 //
 // Created by kiana on 2021/3/22.
 //
 
-#ifndef RM_COMMON_TARGET_COST_FUNCTION_H_
-#define RM_COMMON_TARGET_COST_FUNCTION_H_
+#pragma once
 
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
@@ -46,27 +45,32 @@
 #include <map>
 #include "rm_common/referee/data.h"
 
-namespace rm_common {
-struct TargetState {
+namespace rm_common
+{
+struct TargetState
+{
   int id{};
   double pos_x{}, pos_y{}, pos_z{};
   double vel_x{}, vel_y{}, vel_z{};
   ros::Time last_receive_;
 };
 
-class TargetCostFunction {
- public:
-  explicit TargetCostFunction(ros::NodeHandle &nh, const RefereeData &referee_data);
-  double costFunction(const rm_msgs::TrackDataArray &track_data_array);
-  double costFunction(const TargetState &target_state);
-  int getId() const { return optimal_id_; };
+class TargetCostFunction
+{
+public:
+  explicit TargetCostFunction(ros::NodeHandle& nh, const RefereeData& referee_data);
+  double costFunction(const rm_msgs::TrackDataArray& track_data_array);
+  double costFunction(const TargetState& target_state);
+  int getId() const
+  {
+    return optimal_id_;
+  };
 
- private:
+private:
   double k_pos_{}, k_vel_{}, k_hp_{}, k_freq_{}, timeout_{};
-  const RefereeData &referee_data_;
+  const RefereeData& referee_data_;
   int optimal_id_{};
   std::map<int, TargetState> id2target_states_;
   ros::Time last_switch_target_;
 };
-}
-#endif // RM_COMMON_TARGET_COST_FUNCTION_H_
+}  // namespace rm_common
