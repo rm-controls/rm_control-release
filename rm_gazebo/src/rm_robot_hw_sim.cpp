0135d92ea303837306c1b8783bc74457a2c017ec
qiayuan <liaoqiayuan@gmail.com>
2021-09-26 04:00:13 +0000

    Rebase from 'upstream'

--- rm_gazebo/src/rm_robot_hw_sim.cpp
+++ rm_gazebo/src/rm_robot_hw_sim.cpp
@@ -47,17 +47,107 @@ bool RmRobotHWSim::initSim(const std::string& robot_namespace, ros::NodeHandle m
 {
   bool ret = DefaultRobotHWSim::initSim(robot_namespace, model_nh, parent_model, urdf_model, transmissions);
   gazebo_ros_control::DefaultRobotHWSim::registerInterface(&robot_state_interface_);
+  gazebo_ros_control::DefaultRobotHWSim::registerInterface(&imu_sensor_interface_);
+  XmlRpc::XmlRpcValue xml_rpc_value;
+
+  if (!model_nh.getParam("imus", xml_rpc_value))
+    ROS_WARN("No imu specified");
+  else
+    parseImu(xml_rpc_value, parent_model);
+  world_ = parent_model->GetWorld();  // For gravity
   return ret;
 }
 
 void RmRobotHWSim::readSim(ros::Time time, ros::Duration period)
 {
   gazebo_ros_control::DefaultRobotHWSim::readSim(time, period);
+  for (auto& imu : imu_datas_)
+  {
+    // TODO(qiayuan) Add noise
+    ignition::math::Pose3d pose = imu.link_prt->WorldPose();
+    imu.ori[0] = pose.Rot().X();
+    imu.ori[1] = pose.Rot().Y();
+    imu.ori[2] = pose.Rot().Z();
+    imu.ori[3] = pose.Rot().W();
+    ignition::math::Vector3d rate = imu.link_prt->RelativeAngularVel();
+    imu.angular_vel[0] = rate.X();
+    imu.angular_vel[1] = rate.Y();
+    imu.angular_vel[2] = rate.Z();
+    ignition::math::Vector3d accel = imu.link_prt->RelativeLinearAccel();
+    // TODO(qiayuan): Add gravity
+    // https://github.com/tu-darmstadt-ros-pkg/hector_gazebo/blob/melodic-devel/hector_gazebo_plugins/src/gazebo_ros_imu.cpp
+    imu.linear_acc[0] = accel.X();
+    imu.linear_acc[1] = accel.Y();
+    imu.linear_acc[2] = accel.Z();
+  }
+
   // Set cmd to zero to avoid crazy soft limit oscillation when not controller loaded
   for (auto& cmd : joint_effort_command_)
     cmd = 0;
   for (auto& cmd : joint_velocity_command_)
     cmd = 0;
+}
+
+void RmRobotHWSim::parseImu(XmlRpc::XmlRpcValue& imu_datas, const gazebo::physics::ModelPtr& parent_model)
+{
+  ROS_ASSERT(imu_datas.getType() == XmlRpc::XmlRpcValue::TypeStruct);
+  for (auto it = imu_datas.begin(); it != imu_datas.end(); ++it)
+  {
+    if (!it->second.hasMember("frame_id"))
+    {
+      ROS_ERROR_STREAM("Imu " << it->first << " has no associated frame id.");
+      continue;
+    }
+    else if (!it->second.hasMember("orientation_covariance_diagonal"))
+    {
+      ROS_ERROR_STREAM("Imu " << it->first << " has no associated orientation covariance diagonal.");
+      continue;
+    }
+    else if (!it->second.hasMember("angular_velocity_covariance"))
+    {
+      ROS_ERROR_STREAM("Imu " << it->first << " has no associated angular velocity covariance.");
+      continue;
+    }
+    else if (!it->second.hasMember("linear_acceleration_covariance"))
+    {
+      ROS_ERROR_STREAM("Imu " << it->first << " has no associated linear acceleration covariance.");
+      continue;
+    }
+    XmlRpc::XmlRpcValue ori_cov = imu_datas[it->first]["orientation_covariance_diagonal"];
+    ROS_ASSERT(ori_cov.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    ROS_ASSERT(ori_cov.size() == 3);
+    for (int i = 0; i < ori_cov.size(); ++i)
+      ROS_ASSERT(ori_cov[i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+    XmlRpc::XmlRpcValue angular_cov = imu_datas[it->first]["orientation_covariance_diagonal"];
+    ROS_ASSERT(angular_cov.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    ROS_ASSERT(angular_cov.size() == 3);
+    for (int i = 0; i < angular_cov.size(); ++i)
+      ROS_ASSERT(angular_cov[i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+    XmlRpc::XmlRpcValue linear_cov = imu_datas[it->first]["linear_acceleration_covariance"];
+    ROS_ASSERT(linear_cov.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    ROS_ASSERT(linear_cov.size() == 3);
+    for (int i = 0; i < linear_cov.size(); ++i)
+      ROS_ASSERT(linear_cov[i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+
+    std::string frame_id = imu_datas[it->first]["frame_id"];
+    gazebo::physics::LinkPtr link_ptr = parent_model->GetLink(frame_id);
+    ROS_ASSERT(link_ptr != nullptr);
+    imu_datas_.push_back((ImuData{
+        .link_prt = link_ptr,
+        .ori = { 0., 0., 0., 0. },
+        .ori_cov = { static_cast<double>(ori_cov[0]), 0., 0., 0., static_cast<double>(ori_cov[1]), 0., 0., 0.,
+                     static_cast<double>(ori_cov[2]) },
+        .angular_vel = { 0., 0., 0. },
+        .angular_vel_cov = { static_cast<double>(angular_cov[0]), 0., 0., 0., static_cast<double>(angular_cov[1]), 0.,
+                             0., 0., static_cast<double>(angular_cov[2]) },
+        .linear_acc = { 0., 0., 0. },
+        .linear_acc_cov = { static_cast<double>(linear_cov[0]), 0., 0., 0., static_cast<double>(linear_cov[1]), 0., 0.,
+                            0., static_cast<double>(linear_cov[2]) } }));
+    ImuData& imu_data = imu_datas_.back();
+    imu_sensor_interface_.registerHandle(
+        hardware_interface::ImuSensorHandle(it->first, frame_id, imu_data.ori, imu_data.ori_cov, imu_data.angular_vel,
+                                            imu_data.angular_vel_cov, imu_data.linear_acc, imu_data.linear_acc_cov));
+  }
 }
 
 }  // namespace rm_gazebo
